e12e792152261ce4b175b01f8610982269263ffc
Kendall MacMillan <kendall.macmillan@newtoneurope.com>
2020-11-17 09:21:10 +0000

    fixed import error with components/Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react"
-import Layout from "../components/layout"
+import React, { Component } from "react";
+import Layout from "../components/Layout";
 
 import classes from './index.module.css';
 
